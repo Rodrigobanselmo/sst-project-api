8431b7bb74763956fe30a0f5000b24838cf2b695
Rodrigo Barbosa Anselmo <rodrigobanselmo@gmail.com>
2021-10-24 14:33:37 -0300

    feat: swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const options = new DocumentBuilder()
+    .setTitle('Refresh Tokens')
+    .setDescription(
+      'Full authentication Rest API with typeorm and NestJs using refresh token strategy ',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3333);
 }
 bootstrap();
