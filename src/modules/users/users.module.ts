b61b9490a20d85a21d2988b49491c6af4876863f
Rodrigo Barbosa Anselmo <rodrigobanselmo@gmail.com>
2021-10-24 16:21:56 -0300

    fix: adding providers to user module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { HashProvider } from '../../shared/providers/HashProvider/implementations/HashProvider';
+import { UsersRepository } from './repositories/implementations/UsersRepository';
 import { CreateUserController } from './useCases/create-user/create-user.controller';
 import { CreateUserService } from './useCases/create-user/create-user.service';
 
 @Module({
   controllers: [CreateUserController],
-  providers: [CreateUserService],
+  providers: [HashProvider, UsersRepository, CreateUserService],
 })
 export class UsersModule {}
