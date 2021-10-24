7c9496b7917bcba83338a85247f198f9ac3bd912
Rodrigo Barbosa Anselmo <rodrigobanselmo@gmail.com>
2021-10-24 15:22:09 -0300

    feat: adding Date and Hash Provider

---
+++ src/shared/providers/HashProvider/implementations/HashProvider.ts
@@ -0,0 +1,14 @@
+import { compare, hash } from 'bcrypt';
+import { IHashProvider } from '../models/IHashProvider';
+
+class HashProvider implements IHashProvider {
+  async createHash(password: string): Promise<string> {
+    const passwordHash = await hash(password, 8);
+    return passwordHash;
+  }
+  async compare(password: string, hash_password: string): Promise<boolean> {
+    return compare(password, hash_password);
+  }
+}
+
+export { HashProvider };
