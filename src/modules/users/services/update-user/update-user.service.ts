5b57a1903c55ca186f85d1d825646dbb7533b0f2
Rodrigo Anselmo <69397304+Rodrigobanselmo@users.noreply.github.com>
2021-11-08 09:14:32 -0300

    feat: created user update

---
+++ src/modules/users/services/update-user/update-user.service.ts
@@ -0,0 +1,49 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { HashProvider } from 'src/shared/providers/HashProvider/implementations/HashProvider';
+
+import { UpdateUserDto } from '../../dto/update-user.dto';
+import { UsersRepository } from '../../repositories/implementations/UsersRepository';
+
+@Injectable()
+export class UpdateUserService {
+  constructor(
+    private readonly userRepository: UsersRepository,
+    private readonly hashProvider: HashProvider,
+  ) {}
+
+  async execute(
+    id: number,
+    { password, oldPassword, ...restUpdateUserDto }: UpdateUserDto,
+  ) {
+    if (!id) throw new BadRequestException(`Bad Request`);
+
+    const updateUserDto: UpdateUserDto = { ...restUpdateUserDto };
+
+    const userData = await this.userRepository.findById(id);
+    if (!userData) throw new NotFoundException(`user #${id} not found`);
+
+    if (password) {
+      if (!oldPassword) throw new BadRequestException(`Old password missing`);
+
+      const passwordMatch = await this.hashProvider.compare(
+        oldPassword,
+        userData.password,
+      );
+
+      if (!passwordMatch) {
+        throw new BadRequestException('password incorrect');
+      }
+
+      const passHash = await this.hashProvider.createHash(password);
+      updateUserDto.password = passHash;
+    }
+
+    const user = await this.userRepository.update(id, updateUserDto);
+
+    return user;
+  }
+}
