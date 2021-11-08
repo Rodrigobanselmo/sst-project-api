bd8a9be7c3b9cb89d423ef99609492559b4ee23c
Rodrigo Anselmo <69397304+Rodrigobanselmo@users.noreply.github.com>
2021-11-08 10:56:31 -0300

    feat: add routes

--- src/modules/users/controller/users.controller.spec.ts
+++ src/modules/users/controller/users.controller.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CreateUserService } from '../services/create-user/create-user.service';
+import { FindByEmailService } from '../services/find-by-email/find-by-email.service';
+import { FindByIdService } from '../services/find-by-id/find-by-id.service';
+import { ResetPasswordService } from '../services/reset-password/reset-password.service';
+import { UpdateUserService } from '../services/update-user/update-user.service';
 import { UsersController } from './users.controller';
 
 describe('ControllerController', () => {
@@ -11,6 +15,30 @@ describe('ControllerController', () => {
       providers: [
         {
           provide: CreateUserService,
+          useValue: {
+            execute: jest.fn().mockResolvedValue({}),
+          },
+        },
+        {
+          provide: UpdateUserService,
+          useValue: {
+            execute: jest.fn().mockResolvedValue({}),
+          },
+        },
+        {
+          provide: ResetPasswordService,
+          useValue: {
+            execute: jest.fn().mockResolvedValue({}),
+          },
+        },
+        {
+          provide: FindByEmailService,
+          useValue: {
+            execute: jest.fn().mockResolvedValue({}),
+          },
+        },
+        {
+          provide: FindByIdService,
           useValue: {
             execute: jest.fn().mockResolvedValue({}),
           },
--- src/modules/users/controller/users.controller.ts
+++ src/modules/users/controller/users.controller.ts
@@ -1,19 +1,65 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { ValidateEmailPipe } from './../../../shared/pipes/validate-email.pipe';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { classToClass } from 'class-transformer';
 import { Public } from '../../../shared/decorators/public.decorator';
 
 import { CreateUserDto } from '../dto/create-user.dto';
+import { ResetPasswordDto } from '../dto/reset-pass';
+import { UpdateUserDto } from '../dto/update-user.dto';
 import { CreateUserService } from '../services/create-user/create-user.service';
+import { FindByEmailService } from '../services/find-by-email/find-by-email.service';
+import { FindByIdService } from '../services/find-by-id/find-by-id.service';
+import { ResetPasswordService } from '../services/reset-password/reset-password.service';
+import { UpdateUserService } from '../services/update-user/update-user.service';
 
 @ApiTags('users')
 @Controller('users')
 export class UsersController {
-  constructor(private readonly createUserService: CreateUserService) {}
+  constructor(
+    private readonly createUserService: CreateUserService,
+    private readonly resetPasswordService: ResetPasswordService,
+    private readonly updateUserService: UpdateUserService,
+    private readonly findByEmailService: FindByEmailService,
+    private readonly findByIdService: FindByIdService,
+  ) {}
+
+  @Get(':id')
+  findId(@Param('id', ParseIntPipe) id: number) {
+    return classToClass(this.findByIdService.execute(+id));
+  }
+
+  @Get()
+  findEmail(@Query('email', ValidateEmailPipe) email: string) {
+    return classToClass(this.findByEmailService.execute(email));
+  }
 
   @Public()
-  @Post('create')
+  @Post()
   async create(@Body() createUserDto: CreateUserDto) {
     return classToClass(this.createUserService.execute(createUserDto));
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return classToClass(this.updateUserService.execute(+id, updateUserDto));
+  }
+
+  @Public()
+  @Patch('reset-password')
+  async reset(@Body() resetPasswordDto: ResetPasswordDto) {
+    return classToClass(this.resetPasswordService.execute(resetPasswordDto));
   }
 }
