b92d22790153b9b11732af0838318ad49b789804
Rodrigo Anselmo <69397304+Rodrigobanselmo@users.noreply.github.com>
2021-11-08 10:55:58 -0300

    feat: inject providers users

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -6,10 +6,22 @@ import { UsersRepository } from './repositories/implementations/UsersRepository'
 import { CreateUserService } from './services/create-user/create-user.service';
 import { UpdateUserService } from './services/update-user/update-user.service';
 import { ResetPasswordService } from './services/reset-password/reset-password.service';
+import { FindByIdService } from './services/find-by-id/find-by-id.service';
+import { FindByEmailService } from './services/find-by-email/find-by-email.service';
+import { RefreshTokensRepository } from '../auth/repositories/implementations/RefreshTokensRepository';
 
 @Module({
   controllers: [UsersController],
-  providers: [HashProvider, UsersRepository, CreateUserService, UpdateUserService, ResetPasswordService],
+  providers: [
+    HashProvider,
+    UsersRepository,
+    CreateUserService,
+    UpdateUserService,
+    ResetPasswordService,
+    FindByIdService,
+    FindByEmailService,
+    RefreshTokensRepository,
+  ],
   exports: [UsersRepository],
 })
 export class UsersModule {}
