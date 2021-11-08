ad235fb0a2badb37810b6adb8846a54c8bac18dd
Rodrigo Anselmo <69397304+Rodrigobanselmo@users.noreply.github.com>
2021-11-08 09:15:29 -0300

    feat: user reset pass service

---
+++ src/modules/users/services/reset-password/reset-password.service.ts
@@ -0,0 +1,29 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { RefreshTokensRepository } from '../../../../modules/auth/repositories/implementations/RefreshTokensRepository';
+
+import { HashProvider } from '../../../../shared/providers/HashProvider/implementations/HashProvider';
+import { ResetPasswordDto } from '../../dto/reset-pass';
+import { UsersRepository } from '../../repositories/implementations/UsersRepository';
+
+@Injectable()
+export class ResetPasswordService {
+  constructor(
+    private readonly userRepository: UsersRepository,
+    private readonly refreshTokensRepository: RefreshTokensRepository,
+    private readonly hashProvider: HashProvider,
+  ) {}
+
+  async execute({ tokenId, password }: ResetPasswordDto) {
+    const refresh_token = await this.refreshTokensRepository.findById(tokenId);
+
+    if (!refresh_token) throw new NotFoundException('Token not found');
+
+    const passHash = await this.hashProvider.createHash(password);
+
+    const user = await this.userRepository.update(refresh_token.userId, {
+      password: passHash,
+    });
+
+    return user;
+  }
+}
