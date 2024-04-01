5aa143720307adace70f045cf2964204c3774908
Rodrigo Barbosa Anselmo <rodrigobanselmo@gmail.com>
2024-04-01 18:00:44 -0300

    feat? cascade file delete

--- prisma/run/realiza-cover.ts
+++ prisma/run/realiza-cover.ts
@@ -5,10 +5,10 @@ import { arrayChunks } from '../../src/shared/utils/arrayChunks';
 const realizaId = '7afc3b79-f001-4587-aa21-062d2fb37f6a';
 const jsonCover = {
   coverProps: {
-    logoProps: { maxLogoHeight: 141, y: 78, maxLogoWidth: 212, x: 210 },
+    logoProps: { maxLogoHeight: 141, y: 58, maxLogoWidth: 212, x: 200 },
     backgroundImagePath: 'images/cover/realiza.png',
     titleProps: {
-      x: 163,
+      x: 103,
       y: 310,
       boxX: 464,
       boxY: 0,
@@ -16,7 +16,7 @@ const jsonCover = {
       color: 'FFFFFF',
     },
     versionProps: {
-      x: 163,
+      x: 103,
       y: 480,
       boxX: 464,
       boxY: 0,
@@ -24,7 +24,7 @@ const jsonCover = {
       size: 14,
     },
     companyProps: {
-      x: 163,
+      x: 103,
       y: 510,
       boxX: 464,
       boxY: 0,
@@ -35,20 +35,20 @@ const jsonCover = {
 };
 
 export const realizaCover = async (prisma: PrismaClient) => {
-  await prisma.documentCover.create({
-    data: {
-      json: jsonCover,
-      companyId: realizaId,
-    },
-  });
-  // const realiza = await prisma.documentCover.findFirst({
-  //   where: { companyId: realizaId },
-  // });
-  // await prisma.documentCover.update({
+  // await prisma.documentCover.create({
   //   data: {
   //     json: jsonCover,
   //     companyId: realizaId,
   //   },
-  //   where: { id: realiza.id },
   // });
+  const realiza = await prisma.documentCover.findFirst({
+    where: { companyId: realizaId },
+  });
+  await prisma.documentCover.update({
+    data: {
+      json: jsonCover,
+      companyId: realizaId,
+    },
+    where: { id: realiza.id },
+  });
 };
