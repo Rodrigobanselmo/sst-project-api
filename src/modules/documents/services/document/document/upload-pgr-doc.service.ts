0b7042d8b344e8a64ec4723cd0810ac3b6d7f77d
Rodrigo Barbosa Anselmo <rodrigobanselmo@gmail.com>
2024-11-11 12:54:46 -0300

    fix: pgr old

--- src/modules/documents/services/document/document/upload-pgr-doc.service.ts
+++ src/modules/documents/services/document/document/upload-pgr-doc.service.ts
@@ -1,15 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
-import { CreatorDocumentPGR } from '@/@v2/documents/application/factories/document/creators/document-pgr/document-pgr.creator';
 import { UploadDocumentDto } from '../../../dto/document.dto';
+import { DocumentPGRFactory } from '../../../factories/document/products/PGR/DocumentPGRFactory';
 
 @Injectable()
 export class PgrUploadService {
-  constructor(private readonly creatorDocumentPGR: CreatorDocumentPGR) { }
+  constructor(private readonly documentPGRFactory: DocumentPGRFactory) { }
   async execute(body: UploadDocumentDto) {
-
-    return await this.creatorDocumentPGR.execute({
-      documentVersionId: body.id,
-    });
+    return await this.documentPGRFactory.execute(body);
   }
 }
