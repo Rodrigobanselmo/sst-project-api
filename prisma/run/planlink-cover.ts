46477dced66c67ea33b43dcf8ff767d45d34b904
Rodrigo Barbosa Anselmo <rodrigobanselmo@gmail.com>
2026-01-17 15:25:00 -0600

    feat: add planlink cover generation with JSON configuration

---
+++ prisma/run/planlink-cover.ts
@@ -0,0 +1,54 @@
+import { asyncEach } from '../../src/shared/utils/asyncEach';
+import { PrismaClient } from '@prisma/client';
+import { arrayChunks } from '../../src/shared/utils/arrayChunks';
+
+const planlinkId = '73914bc5-91af-4236-84f2-b76c2419fde7';
+const jsonCover = {
+  coverProps: {
+    logoProps: { maxLogoHeight: 141, y: 58, maxLogoWidth: 212, x: 200 },
+    backgroundImagePath: 'images/cover/planlink.png',
+    titleProps: {
+      x: 103,
+      y: 310,
+      boxX: 464,
+      boxY: 0,
+      size: 28,
+      color: 'FFFFFF',
+    },
+    versionProps: {
+      x: 103,
+      y: 480,
+      boxX: 464,
+      boxY: 0,
+      color: 'FFFFFF',
+      size: 14,
+    },
+    companyProps: {
+      x: 103,
+      y: 510,
+      boxX: 464,
+      boxY: 0,
+      color: 'FFFFFF',
+      size: 14,
+    },
+  },
+};
+
+export const planlinkCover = async (prisma: PrismaClient) => {
+  // await prisma.documentCover.create({
+  //   data: {
+  //     json: jsonCover,
+  //     companyId: realizaId,
+  //   },
+  // });
+  const planlink = await prisma.documentCover.findFirst({
+    where: { companyId: planlinkId },
+  });
+  await prisma.documentCover.update({
+    data: {
+      json: jsonCover,
+      companyId: planlinkId,
+    },
+    where: { id: planlink?.id ?? 0 },
+  });
+};
