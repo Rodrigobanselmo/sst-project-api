57ed313216d606538fb7d216723ba98bc3337159
Rodrigo Barbosa Anselmo <rodrigobanselmo@gmail.com>
2026-04-08 12:31:14 -0300

    fix(database): increase MAX_LIMIT from 100 to 1000 to support larger batch operations

--- src/@v2/shared/utils/database/get-pagination.ts
+++ src/@v2/shared/utils/database/get-pagination.ts
@@ -1,20 +1,19 @@
-const DEFAULT_PAGE = 1
-const DEFAULT_LIMIT = 20
-const DEFAULT_OFFSET = 0
-const MAX_LIMIT = 100
-
+const DEFAULT_PAGE = 1;
+const DEFAULT_LIMIT = 20;
+const DEFAULT_OFFSET = 0;
+const MAX_LIMIT = 1000;
 
 export function getPagination(page: number = DEFAULT_PAGE, limit: number = DEFAULT_LIMIT) {
-  limit = getMaxLimit(limit)
-  return { limit, offSet: getOffset(page, limit), page }
+  limit = getMaxLimit(limit);
+  return { limit, offSet: getOffset(page, limit), page };
 }
 
 function getMaxLimit(limit: number): number {
-  return limit > MAX_LIMIT ? MAX_LIMIT : limit
+  return limit > MAX_LIMIT ? MAX_LIMIT : limit;
 }
 
 function getOffset(page: number, limit: number): number {
-  if (!page) return DEFAULT_OFFSET
+  if (!page) return DEFAULT_OFFSET;
 
-  return limit * (page - 1)
+  return limit * (page - 1);
 }
